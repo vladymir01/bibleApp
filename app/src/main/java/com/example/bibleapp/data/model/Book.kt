db3c7544a55cb4d79f8009f903168a7b5a191af2
vladymir01 <adamvladymir@gmail.com>
2023-10-15 18:52:00 -0400

    First commit

---
+++ app/src/main/java/com/example/bibleapp/data/model/Book.kt
@@ -0,0 +1,9 @@
+package com.example.bibleapp.data.model
+
+data class Book(
+    val abbreviation: String,
+    val bibleId: String,
+    val id: String,
+    val name: String,
+    val nameLong: String
+)
